d76bf60d1f41d5de3dfc5ca48c7c832e8ff3bbce
Oliver Evans <oliverevans96@gmail.com>
2022-04-11 01:00:55 -0700

    Refactor nix poetry overrides

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
       let
         pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux;
         # Inpsired by https://github.com/nix-community/poetry2nix/issues/218#issuecomment-981615612
-        addPythonDeps = self: super: builtins.mapAttrs (pkgName: pyDeps:
+        addPythonDeps = super: builtins.mapAttrs (pkgName: pyDeps:
           super."${pkgName}".overridePythonAttrs (
             old: {
-              buildInputs = (old.buildInputs or []) ++ map (depName: self."${depName}") pyDeps;
+              buildInputs = (old.buildInputs or []) ++ pyDeps;
             }
           )
         );
@@ -21,6 +21,8 @@
             }
           )
         );
+        mkOverrides = { pythonDeps, nativeDeps }: super:
+          (addPythonDeps super pythonDeps) // (addNativeDeps super nativeDeps);
         myShell = pkgs.poetry2nix.mkPoetryEnv {
           projectDir = ./.;
           extraPackages = (ps: with ps; [ python-lsp-server ]);
@@ -28,16 +30,17 @@
           editablePackageSources = {
             my-app = ./.;
           };
-          overrides = self: super:
-            addPythonDeps self super {
-              rsa = [ "poetry" ];
-              soupsieve = [ "hatchling" ];
-              tomli = [ "flit-core" ];
-              pyparsing = [ "flit-core" ];
-            }
-            // addNativeDeps super {
-              lxml = with pkgs; [ libxml2 libxslt ];
+          overrides = self: mkOverrides {
+            pythonDeps = with self; {
+              rsa = [ poetry ];
+              soupsieve = [ hatchling ];
+              tomli = [ flit-core ];
+              pyparsing = [ flit-core ];
             };
+            nativeDeps = with pkgs; {
+              lxml = [ libxml2 libxslt ];
+            };
+          };
         };
       in
       flake-utils.lib.eachDefaultSystem
